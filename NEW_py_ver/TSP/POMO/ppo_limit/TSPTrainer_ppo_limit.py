b19368eb33bf684543f8d926e35803733f9e247a
yuzm1 <yuzm1@xiaopeng.com>
2025-07-17 11:27:43 +0800

    yzm update model: reinforce_avg, reinforce_max, ppo, ppo_limit

---
+++ NEW_py_ver/TSP/POMO/ppo_limit/TSPTrainer_ppo_limit.py
@@ -0,0 +1,224 @@
+
+import torch
+from logging import getLogger
+
+from TSPEnv import TSPEnv as Env
+from TSPModel_ppo_limit import TSPModel_ppo_limit as Model
+
+from torch.optim import Adam as Optimizer
+from torch.optim.lr_scheduler import MultiStepLR as Scheduler
+
+from utils.utils import *
+
+torch.set_printoptions(profile="full")
+
+class TSPTrainer_ppo_limit:
+    def __init__(self,
+                 env_params,
+                 model_params,
+                 optimizer_params,
+                 trainer_params):
+
+        # save arguments
+        self.env_params = env_params
+        self.model_params = model_params
+        self.optimizer_params = optimizer_params
+        self.trainer_params = trainer_params
+
+        # result folder, logger
+        self.logger = getLogger(name='trainer')
+        self.result_folder = get_result_folder()
+        self.result_log = LogData()
+
+        # cuda
+        USE_CUDA = self.trainer_params['use_cuda']
+        if USE_CUDA:
+            cuda_device_num = self.trainer_params['cuda_device_num']
+            torch.cuda.set_device(cuda_device_num)
+            device = torch.device('cuda', cuda_device_num)
+            torch.set_default_tensor_type('torch.cuda.FloatTensor')
+        else:
+            device = torch.device('cpu')
+            torch.set_default_tensor_type('torch.FloatTensor')
+
+        # Main Components
+        self.model = Model(**self.model_params)
+        self.env = Env(**self.env_params)
+        self.optimizer = Optimizer(self.model.parameters(), **self.optimizer_params['optimizer'])
+        self.scheduler = Scheduler(self.optimizer, **self.optimizer_params['scheduler'])
+
+        self.logger.info("Model: ")
+        self.logger.info(self.model)
+        # Restore
+        self.start_epoch = 1
+        model_load = trainer_params['model_load']
+        if model_load['enable']:
+            checkpoint_fullname = '{path}/checkpoint-{epoch}.pt'.format(**model_load)
+            checkpoint = torch.load(checkpoint_fullname, map_location=device)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.start_epoch = 1 + model_load['epoch']
+            self.result_log.set_raw_data(checkpoint['result_log'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            self.scheduler.last_epoch = model_load['epoch']-1
+            self.logger.info('Saved Model Loaded !!')
+
+        # utility
+        self.time_estimator = TimeEstimator()
+
+    def run(self):
+        self.time_estimator.reset(self.start_epoch)
+        for epoch in range(self.start_epoch, self.trainer_params['epochs']+1):
+            self.logger.info('=================================================================')
+
+            # LR Decay
+            self.scheduler.step()
+
+            # Train
+            train_score, train_loss = self._train_one_epoch(epoch)
+            self.result_log.append('train_score', epoch, train_score)
+            self.result_log.append('train_loss', epoch, train_loss)
+
+            ############################
+            # Logs & Checkpoint
+            ############################
+            elapsed_time_str, remain_time_str = self.time_estimator.get_est_string(epoch, self.trainer_params['epochs'])
+            self.logger.info("Epoch {:3d}/{:3d}: Time Est.: Elapsed[{}], Remain[{}]".format(
+                epoch, self.trainer_params['epochs'], elapsed_time_str, remain_time_str))
+
+            all_done = (epoch == self.trainer_params['epochs'])
+            model_save_interval = self.trainer_params['logging']['model_save_interval']
+            img_save_interval = self.trainer_params['logging']['img_save_interval']
+
+            if epoch > 1:  # save latest images, every epoch
+                self.logger.info("Saving log_image")
+                image_prefix = '{}/latest'.format(self.result_folder)
+                util_save_log_image_with_label(image_prefix, self.trainer_params['logging']['log_image_params_1'],
+                                    self.result_log, labels=['train_score'])
+                util_save_log_image_with_label(image_prefix, self.trainer_params['logging']['log_image_params_2'],
+                                    self.result_log, labels=['train_loss'])
+
+            if all_done or (epoch % model_save_interval) == 0:
+                self.logger.info("Saving trained_model")
+                checkpoint_dict = {
+                    'epoch': epoch,
+                    'model_state_dict': self.model.state_dict(),
+                    'optimizer_state_dict': self.optimizer.state_dict(),
+                    'scheduler_state_dict': self.scheduler.state_dict(),
+                    'result_log': self.result_log.get_raw_data()
+                }
+                torch.save(checkpoint_dict, '{}/checkpoint-{}.pt'.format(self.result_folder, epoch))
+
+            if all_done or (epoch % img_save_interval) == 0:
+                image_prefix = '{}/img/checkpoint-{}'.format(self.result_folder, epoch)
+                util_save_log_image_with_label(image_prefix, self.trainer_params['logging']['log_image_params_1'],
+                                    self.result_log, labels=['train_score'])
+                util_save_log_image_with_label(image_prefix, self.trainer_params['logging']['log_image_params_2'],
+                                    self.result_log, labels=['train_loss'])
+
+            if all_done:
+                self.logger.info(" *** Training Done *** ")
+                self.logger.info("Now, printing log array...")
+                util_print_log_array(self.logger, self.result_log)
+
+    def _train_one_epoch(self, epoch):
+
+        score_AM = AverageMeter()
+        loss_AM = AverageMeter()
+
+        train_num_episode = self.trainer_params['train_episodes']
+        episode = 0
+        loop_cnt = 0
+        while episode < train_num_episode:
+
+            remaining = train_num_episode - episode
+            batch_size = min(self.trainer_params['train_batch_size'], remaining)
+
+            avg_score, avg_loss = self._train_one_batch(batch_size)
+            score_AM.update(avg_score, batch_size)
+            loss_AM.update(avg_loss, batch_size)
+
+            episode += batch_size
+
+            # Log First 10 Batch, only at the first epoch
+            if epoch == self.start_epoch:
+                loop_cnt += 1
+                if loop_cnt <= 10:
+                    self.logger.info('Epoch {:3d}: Train {:3d}/{:3d}({:1.1f}%)  Score: {:.4f},  Loss: {:.4f}'
+                                     .format(epoch, episode, train_num_episode, 100. * episode / train_num_episode,
+                                             score_AM.avg, loss_AM.avg))
+
+        # Log Once, for each epoch
+        self.logger.info('Epoch {:3d}: Train ({:3.0f}%)  Score: {:.4f},  Loss: {:.4f}'
+                         .format(epoch, 100. * episode / train_num_episode,
+                                 score_AM.avg, loss_AM.avg))
+
+        return score_AM.avg, loss_AM.avg
+
+    def _train_one_batch(self, batch_size):
+
+        # Prep
+        ###############################################
+        self.model.train()
+        self.env.load_problems(batch_size)
+        reset_state, _, _ = self.env.reset()
+        self.model.pre_forward(reset_state)
+
+        prob_list = torch.zeros(size=(batch_size, self.env.pomo_size, 0))
+        # shape: (batch, pomo, 0~problem)
+
+        # POMO Rollout
+        ###############################################
+        state, reward, done = self.env.pre_step()
+        while not done: #生成一个解：完整轨迹
+            selected, prob = self.model(state)  #一次迭代：选一个点，给出选这个点的概率; a, π（a|s）= Actor(s)
+            # shape: (batch, pomo)
+            state, reward, done = self.env.step(selected)  #跟环境交互，得到s', r = Critic(s, a)，更新环境
+            prob_list = torch.cat((prob_list, prob[:, :, None]), dim=2) #收集每个a的π（a|s）
+            #将 prob_list 和扩展维度后的 prob 沿着第三个维度（dim=2）进行拼接。
+            #假设 prob_list 的形状为 (batch_size, pomo_size, t)，那么拼接后的 prob_list 的形状将变为 (batch_size, pomo_size, t + 1)。
+
+        # Loss
+        ###############################################
+        advantage = reward.float().max(dim=1, keepdims=True)[0] - reward #公式中的序号2，当前轨迹的奖励和平均奖励之差
+        # shape: (batch, pomo)
+        max_index = reward.argmax(dim=1, keepdims=True)
+
+        # self.logger.info("max Index: {}, reward: {}, advantage: {}".format(max_index, reward, advantage))
+
+        log_prob = prob_list.log().sum(dim=2) #公式中的序号3.表示每个样本的每个模式(pomo)下所有时间步(t = problem_size)的对数概率之和。
+        # size = (batch, pomo)
+
+        log_prob_2 = log_prob.detach() 
+
+        dalte_log_prob =  log_prob_2 - torch.gather(log_prob, dim=1, index=max_index)
+
+        ratio = torch.exp(dalte_log_prob) 
+
+        loss_src = advantage * ratio  # Minus Sign: To Increase REWARD
+        eps = 0.3
+        loss_limit = advantage * torch.clamp(ratio, 1 - eps, 1 + eps)
+
+        loss = torch.min(loss_src, loss_limit)
+
+        # self.logger.info("loss: {}, loss_src: {}, loss_limit: {}".format(loss, loss_src, loss_limit))
+
+        # if torch.equal(loss, loss_src):
+        #     self.logger.info("Using loss_src")
+        # elif torch.equal(loss, loss_limit):
+        #     self.logger.info("Using loss_limit")
+        
+        # shape: (batch, pomo)
+        loss_mean = loss.mean()  #公式中的序号1 
+
+        # Score
+        ###############################################
+        max_pomo_reward, _ = reward.max(dim=1)  # get best results from pomo
+        # shape: (batch,)
+        score_mean = -max_pomo_reward.float().mean()  # negative sign to make positive value
+
+        # Step & Return
+        ###############################################
+        self.model.zero_grad()
+        loss_mean.backward()
+        self.optimizer.step()
+        return score_mean.item(), loss_mean.item()
